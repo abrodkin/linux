3e8533dfef5ca43803c7dabe1675b1abfcb166c9
Juergen Beisert <jbe@pengutronix.de>
2014-05-24 23:47:06 +0400

    s3c2410fb: Don't forget to remove the device attribute
    Signed-off-by: Juergen Beisert <jbe@pengutronix.de>

--- drivers/video/s3c2410fb.c
+++ drivers/video/s3c2410fb.c
@@ -1049,6 +1049,7 @@ static int s3c2410fb_remove(struct platform_device *pdev)
 	iounmap(info->io);
 
 	release_mem_region(info->mem->start, resource_size(info->mem));
+	device_remove_file(&pdev->dev, &dev_attr_debug);
 
 	framebuffer_release(fbinfo);
 
