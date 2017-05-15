82b0475b1eb6254580e6c3ab706307db2d23097e
Alexey Brodkin <abrodkin@synopsys.com>
2017-05-15 18:24:05 +0200

    LOCAL: Add fixup for CREG_PAE
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>

--- arch/arc/plat-hsdk/platform.c
+++ arch/arc/plat-hsdk/platform.c
@@ -11,6 +11,7 @@
 #include <linux/init.h>
 #include <linux/smp.h>
 #include <asm/arcregs.h>
+#include <asm/io.h>
 #include <asm/mach_desc.h>
 
 /*
@@ -23,9 +24,28 @@ void relocate_iccm(void)
 		write_aux_reg(ARC_REG_AUX_ICCM, 0x60000000);
 }
 
+/*
+ * Default configuration of PAE regs doesn't work for us causeing
+ * problems with DMA to/from peripherals even if PAE40 is not used.
+ */
+void fixup_pae_regs(void)
+{
+#define ARC_PERIPHERAL_BASE	0xf0000000
+#define	CREG_BASE	(ARC_PERIPHERAL_BASE + 0x1000)
+#define	CREG_PAE	(CREG_BASE + 0x180)
+#define	CREG_PAE_UPDATE	(CREG_BASE + 0x194)
+
+	/* Default is 1, which means "PAE offset = 4GByte" */
+	writel(0, (void __iomem *) CREG_PAE);
+
+	/* Really apply settings made above */
+	writel(1, (void __iomem *) CREG_PAE_UPDATE);
+}
+
 static void hsdk_early_init(void)
 {
 	relocate_iccm();
+	fixup_pae_regs();
 }
 
 static void hsdk_init_per_cpu(unsigned int cpu)
@@ -48,4 +68,6 @@ static const char *hsdk_compat[] __initconst = {
 
 MACHINE_START(SIMULATION, "hsdk")
 	.dt_compat	= hsdk_compat,
+	.init_early	= hsdk_early_init,
+	.init_per_cpu	= hsdk_init_per_cpu,
 MACHINE_END
